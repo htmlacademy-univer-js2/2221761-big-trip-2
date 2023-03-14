14838bcf3a873300f99400e82ecaf13f6b11a7a4
Ilya Konoplitsky <96144903+IlyaKonoplitsky@users.noreply.github.com>
2023-03-14 16:25:01 +0500

    added mock data

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import FiltersView from './view/filters.js';
 import TripEventsPresenter from './presenter/trip.js';
-import { render, RenderPosition } from './render.js';
+import { render } from './render.js';
+import PointsModel from './model/point-model.js';
+import { getPoints, getDestinations, getOffersByType } from './mock/points';
 
-const filterContainer = document.querySelector('.trip-main');
-const tripContaier = document.querySelector('.trip-events');
-const tripPresenter = new TripEventsPresenter();
+const siteHeaderElement = document.querySelector('.trip-main');
+const siteMainElement = document.querySelector('.page-main');
+const tripPresenter = new TripEventsPresenter(siteMainElement.querySelector('.trip-events'));
 
-render(new FiltersView(), filterContainer.querySelector('.trip-controls__filters'), RenderPosition.BEFOREEND);
+const points = getPoints();
+const offersByType = getOffersByType();
+const destinations = getDestinations();
 
-tripPresenter.init(tripContaier);
+const pointsModel = new PointsModel();
+
+render(new FiltersView(), siteHeaderElement.querySelector('.trip-controls__filters'));
+
+pointsModel.init(points, destinations, offersByType);
+tripPresenter.init(pointsModel);
