fd2378adbf60f9212f77cbc59e272ee5145a05d3
Ilya Konoplitsky <96144903+IlyaKonoplitsky@users.noreply.github.com>
2023-06-11 15:41:17 +0500

    Additional functionality

--- src/model/offers-model.js
+++ src/model/offers-model.js
@@ -10,6 +10,8 @@ export default class OffersModel extends Obserbvable {
     this.#pointsApiService = pointsApiService;
   }
 
+  get offers () {return this.#offers;}
+
   init = async () => {
     try {
       this.#offers = await this.#pointsApiService.offers;
@@ -19,7 +21,5 @@ export default class OffersModel extends Obserbvable {
 
     this._notify(UpdateType.INIT);
   };
-
-  get offers () {return this.#offers;}
 
 }
--- src/utils/point.js
+++ src/utils/point.js
@@ -70,6 +70,8 @@ const sortPointsByPrice = (pointA, pointB) => pointB.basePrice - pointA.basePric
 
 const humanizeFormDate = (date) => dayjs(date).format('DD/MM/YY HH:mm');
 
+const humanizeHeaderDate = (date) => dayjs(date).format('MMM D');
+
 const isRightPoint = (point) => dayjs(point.dateFrom).isBefore(dayjs()) && dayjs(point.dateTo).isAfter(dayjs());
 
 const isPastPoint = (point) => dayjs(point.dateTo).isBefore(dayjs()) || isRightPoint(point);
@@ -79,5 +81,7 @@ const isFuturePoint = (point) => dayjs(point.dateFrom).isAfter(dayjs())
 
 const isDatesEqual = (dateA, dateB) => dayjs(dateA).isSame(dateB, 'D');
 
-export{ humanizePointDay, humanizePointTime, humanizeFormDate, getEventDuration, isDatesEqual,
-  isPastPoint, isFuturePoint, getWeightForNullDate, sortPointsByDay, sortPointsByTime, sortPointsByPrice };
+const isMonthsEqual = (dateA, dateB) => dayjs(dateA).isSame(dateB, 'M');
+
+export{ humanizePointDay, humanizePointTime, humanizeFormDate, getEventDuration, isDatesEqual, humanizeHeaderDate,
+  isPastPoint, isFuturePoint, getWeightForNullDate, sortPointsByDay, sortPointsByTime, sortPointsByPrice, isMonthsEqual };
